0c18035d27072b9a4eee435cd0fe681a8060036e
Katie Young <Katie.f.Young@me.com>
2021-07-29 13:50:09 -0400

    Body css is good enough

--- project/frontend/src/components/TestComponent/index.js
+++ project/frontend/src/components/TestComponent/index.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import './TestComponent.css';
+import body from "../../avatarImages/bodies/farmer_boy_base_with_hair.png"
+import shirts from "../../avatarImages/shirts"
+import pants from "../../avatarImages/pants"
 
 function TestComponent() {
 
+    shirts.forEach(element => {
+        console.log('hi!')
+    });
+
     return (
         <>
-            <h1>TEST</h1>
             <div id="bodyTest">
+                <img src={body} id="bodyImg" />
+                <img src={shirt} id="shirtImg" />
+                <img src={pants} id="pantsImg" />
             </div>
         </>
     );
